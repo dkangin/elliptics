69c217726fc805e3256129da02edb3c6c3932f8f
Dmitry <dmitry@lenovo-dkangin.localdomain>
2013-11-08 08:41:13 +0000

    elliptics modification

---
+++ library/original_id_helpers.h
@@ -0,0 +1,26 @@
+#ifndef __ORIGINAL_ID_HELPERS_H
+#define __ORIGINAL_ID_HELPERS_H
+
+#include <eblob/blob.h>
+#include "elliptics.h"
+#include "elliptics/packet.h"
+#include "elliptics/interface.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline void copy_identifiers_cmd (struct eblob_key *src_key, char* original_id)
+{
+	memset((void*)src_key->id, 0, EBLOB_ID_SIZE);
+	int id_size = sizeof (uint64_t);
+	memcpy(src_key->id, original_id + EBLOB_ID_SIZE - sizeof (uint64_t), id_size);
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ORIGINAL_ID_HELPERS_H */
+
