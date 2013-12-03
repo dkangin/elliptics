1a775627ff093582ef646c1b5b7a0b873417663f
Dmitry <dmitry@lenovo-dkangin.localdomain>
2013-12-03 06:46:04 +0000

    Elliptics patches

---
+++ include/elliptics/typedefs.h
@@ -0,0 +1,8 @@
+#ifndef TYPEDEFS_H_VGAAJ
+#define TYPEDEFS_H_VGAAJ 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H 1
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
